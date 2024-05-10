87230704952d428617e90aedea2db91a49b9370f
Alexandre Magno <alexandremagnobezerra@gmail.com>
2024-05-10 14:45:19 -0300

    ajuatei a url do JotaJa

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
       { urlPattern: /^https:\/\/.*\.ola\.click\/.*/, value: "OlaClick" },
       { urlPattern: /^https:\/\/instadelivery\.com\.br\/.*/, value: "InstaDelivery" },
       { urlPattern: /^https:\/\/www\.hubt\.com\.br\/.*/, value: "Hubt" },
-      { urlPattern: /^https:\/\/app\.jotaja\.com\/.*/, value: "Jotaja" },
+      { urlPattern: /^https:\/\/app-core\.jotaja\.com\/.*/, value: "Jotaja" },
       { urlPattern: /^https:\/\/.*\.yooga\.app\/.*/, value: "Yooga" },
       { urlPattern: /^https:\/\/.*\.cardapiodigital\.io\/.*/, value: "CardapioDigital" },
       { urlPattern: /^https:\/\/.*\.saipos\.com\/.*/, value: "Saipos" }
